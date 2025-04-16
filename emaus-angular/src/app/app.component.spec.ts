82482422529a225a8a7e45eb5c537120e0235f4f
Josimar Ferreira <jcfcavile@gmail.com>
2025-04-16 18:05:29 -0300

    criando o projeto frontend

--- emaus-angular/src/app/app.component.spec.ts
+++ emaus-angular/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'emaus-angular' title`, () => {
+  it(`should have as title 'emaus-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('emaus-angular');
